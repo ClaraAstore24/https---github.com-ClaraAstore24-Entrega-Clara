fcbff346bfae637cff8c5303711e2ab03b8caec2
Clara Astore <claraastore13@gmail.com>
2022-11-19 14:26:44 -0300

    URLS

--- EntregaAstore1/EntregaA/views.py
+++ EntregaAstore1/EntregaA/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
 from EntregaA.models import Usuario
 
-def Vista_usuario(request):
-    listado = Usuario.objects.all()
-    Vista = ""
-    for USUARIO in listado:
-        Vista += Usuario.nombre + " | "
 
-    return HttpResponse
+# Create your views here.
+
+
+
+def inicio(request):
+    return HttpResponse("Estas en el inicio")
+
+def usuarios(request):
+    return HttpResponse("Crear usuario")
+
+def voletos_avion(request):
+    return HttpResponse("Estas en voletos de avion")
+
+def viajes_disponibles(request):
+    return HttpResponse("Estas en viajes disponibles")
+
+
+#def Vista_plantilla(request):
+   # listado = Usuario.objects.all()
+   # Vista = ""
+   # for USUARIO in listado:
+    #    Vista += Usuario.nombre + " | "
+
+  #  return HttpResponse
